21121a5593e277f60c5a175081bcbe6c3889ff7a
Muata Nkosi <mnkosi@fandm.edu>
2018-04-22 17:37:26 -0400

    MonoDB initialization with GET request functionality

--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,3 @@ gulp.task('default', function () {
        console.log('Restarting');
    });
 });
-
-// var gulp = require('gulp'),
-//     nodemon = require('gulp-nodemon');
-
-// gulp.task('default', defaultTask);
-
-// function defaultTask(done) {
-//   // place code for your default task here
-//   done();
-// }
